679a7b42604aacb636b1cb20baceec2ce2ce851b
docomo80 <v.sariev@abv.bg>
2020-04-10 02:23:27 +0300

    Changes

---
+++ backend/Models/Student.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let Student = new Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    year: {
+        type: Number
+    }
+
+});
+module.exports = mongoose.model('Student',
+Student);
